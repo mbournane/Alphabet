40adcfc8b301cce4f32936c9846bf5b484597fc1
Bournane Mohamed <mbournane@gmail.com>
2023-06-02 02:37:21 +0200

    Quete caractere

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Alphabet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char c = 'a';
+        Console.WriteLine(c +" = " + "'" +c+ "'");
+        for(int i = 97; i<122;i++)
+        {
+            c++;
+            Console.WriteLine(c +" = " + "'" +c+ "'");
+
+        }
+    }
+}
